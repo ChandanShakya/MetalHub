0730e3bdfabfc4158614e4fecc16b5a62a97cc99
ZXY-CC-3ag13 <ZXY-CC-3ag13@protonmail.com>
2024-08-14 21:13:34 +0545

    refactor: add debugbar and vite reload, fix public path, svg color and footer text

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,27 +18,26 @@ class AppServiceProvider extends ServiceProvider
         $brandAsset = function ($asset) {
             $publicPath = 'public/';
 
-            if (file_exists(public_path($publicPath.$asset))) {
-                return asset($publicPath.$asset);
+            if (file_exists(public_path($publicPath . $asset))) {
+                return asset($publicPath . $asset);
             } else {
-                $type = str($asset)
-                    ->endsWith('.png') ? 'image/png' : 'image/svg+xml';
-
-                return "data:{$type};base64,".base64_encode(file_get_contents('../public/'.$asset));
+                $type = str($asset)->endsWith('.png') ? 'image/png' : 'image/svg+xml';
+                // Use public_path to get the correct path to the public directory
+                return "data:{$type};base64," . base64_encode(file_get_contents(public_path($asset)));
             }
         };
+
         LunarPanel::panel(
-            fn ($panel) => $panel
-            ->brandName('MetalHub')
-            ->path('admin')
-            ->brandLogo($brandAsset('favicon.svg'))
-            ->darkModeBrandLogo($brandAsset('favicon.svg'))
-            ->favicon($brandAsset('favicon.svg'))
-            ->plugins([
-                new ShippingPlugin,
-            ])
-        )
-            ->register();
+            fn($panel) => $panel
+                ->brandName('MetalHub')
+                ->path('admin')
+                ->brandLogo($brandAsset('favicon.svg'))
+                ->darkModeBrandLogo($brandAsset('favicon.svg'))
+                ->favicon($brandAsset('favicon.svg'))
+                ->plugins([
+                    new ShippingPlugin,
+                ])
+        )->register();
     }
 
     /**
