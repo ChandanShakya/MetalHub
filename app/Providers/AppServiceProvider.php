fc0ee385c0a1d7a899680b5375d5c68e3272392f
ChandanShakya <notch0andan@gmail.com>
2024-08-14 16:52:11 +0545

    chore: Update branding and assets for MetalHub

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,8 +15,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $brandAsset = function ($asset) {
+            $publicPath = 'public/';
+
+            if (file_exists(public_path($publicPath.$asset))) {
+                return asset($publicPath.$asset);
+            } else {
+                $type = str($asset)
+                    ->endsWith('.png') ? 'image/png' : 'image/svg+xml';
+
+                return "data:{$type};base64,".base64_encode(file_get_contents('../public/'.$asset));
+            }
+        };
         LunarPanel::panel(
-            fn ($panel) => $panel->path('admin')->plugins([
+            fn ($panel) => $panel
+            ->brandName('MetalHub')
+            ->path('admin')
+            ->brandLogo($brandAsset('favicon.svg'))
+            ->darkModeBrandLogo($brandAsset('favicon.svg'))
+            ->favicon($brandAsset('favicon.svg'))
+            ->plugins([
                 new ShippingPlugin,
             ])
         )
